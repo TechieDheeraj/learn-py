0ed7f15943a10257e3794ec9d61d8de7faeab37f
TechieDheeraj <dheeraj.embdeveloper@gmail.com>
2024-06-26 12:20:49 +0530

    defaultDict sample code

--- collections_intro.py
+++ collections_intro.py
@@ -1,6 +1,10 @@
 from collections import ChainMap
+from collections import defaultdict
 import os
 
 
 def check_collections():
+    d = defaultdict(lambda: "Not present")
+    d['a'] = 1
+    print(d['b'])
     pass
--- lambdas.py
+++ lambdas.py
@@ -5,10 +5,13 @@ def run_lambdas():
     seq = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     result = filter(lambda x: x % 2 != 0, seq)
     print(result, type(result))
+
     numbers = (1, 2, 3, 4)
     numbers1 = (1, 2, 3, 4)
     numbers2 = (5, 6, 7, 8)
     result = map(add, numbers)
     print(list(result))
+
     result1 = map(lambda x, y: x+y, numbers1, numbers2)
     print(list(result1))
+
